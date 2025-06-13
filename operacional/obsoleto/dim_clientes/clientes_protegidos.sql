53cc3321f182d59d9d86457d4afab8f9596bce6d
Bruno Chiaramonti <bchiaramonti@gmail.com>
2025-06-13 16:27:31 -0300

    Deletado arquivos obsoletos

--- operacional/obsoleto/dim_clientes/clientes_protegidos.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE investimentos.clientes_investimentos (
-    id_cliente INT IDENTITY(1,1) PRIMARY KEY,
-    nome NVARCHAR(255) NULL,
-    cod_xp INT NOT NULL,
-    email NVARCHAR(255) NULL,
-    telefone NVARCHAR(255) NULL,
-    data_nascimento DATE NULL,
-    cpf NVARCHAR(255) NULL
-);
