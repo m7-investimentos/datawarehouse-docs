32da4c8c14cf1d19df26352d8237800ca1aa81f3
heitorctm <heitort@gmail.com>
2025-06-20 16:07:19 -0300

    correcao da silver ativacoes

--- operacional/queries/silver/QRY-AHE-002-create_silver_fact_ativacoes_habilitacoes_evasoes.sql
+++ operacional/queries/silver/QRY-AHE-002-create_silver_fact_ativacoes_habilitacoes_evasoes.sql
@@ -2,32 +2,26 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [silver].[fact_resgates](
+CREATE TABLE [silver].[fact_ativacoes_habilitacoes_evasoes](
 	[data_ref] [date] NOT NULL,
-	[conta_xp_cliente] [int] NOT NULL,
-	[cod_assessor] [varchar](50) NOT NULL,
-	[origem_resgate] [varchar](100) NOT NULL,
-	[resgate_bruto_xp] [decimal](18, 2) NOT NULL,
-	[tipo_transferencia] [varchar](100) NOT NULL,
-	[resgate_bruto_transferencia] [decimal](18, 2) NOT NULL,
-	[resgate_bruto_total] [decimal](18, 2) NOT NULL
+	[cod_xp] [int] NOT NULL,
+	[crm_id] [varchar](20) NOT NULL,
+	[id_estrutura] [int] NOT NULL,
+	[faixa_pl] [varchar](50) NULL,
+	[tipo_movimentacao] [varchar](50) NULL
 ) ON [PRIMARY]
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'data de referência do registro' , @level0type=N'SCHEMA',@level0name=N'silver', @level1type=N'TABLE',@level1name=N'fact_resgates', @level2type=N'COLUMN',@level2name=N'data_ref'
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'data de referência do registro' , @level0type=N'SCHEMA',@level0name=N'silver', @level1type=N'TABLE',@level1name=N'fact_ativacoes_habilitacoes_evasoes', @level2type=N'COLUMN',@level2name=N'data_ref'
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'código da conta do cliente no sistema xp' , @level0type=N'SCHEMA',@level0name=N'silver', @level1type=N'TABLE',@level1name=N'fact_resgates', @level2type=N'COLUMN',@level2name=N'conta_xp_cliente'
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'código do cliente no sistema xp' , @level0type=N'SCHEMA',@level0name=N'silver', @level1type=N'TABLE',@level1name=N'fact_ativacoes_habilitacoes_evasoes', @level2type=N'COLUMN',@level2name=N'cod_xp'
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'código do assessor no sistema da xp' , @level0type=N'SCHEMA',@level0name=N'silver', @level1type=N'TABLE',@level1name=N'fact_resgates', @level2type=N'COLUMN',@level2name=N'cod_assessor'
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'identificador do assessor no crm' , @level0type=N'SCHEMA',@level0name=N'silver', @level1type=N'TABLE',@level1name=N'fact_ativacoes_habilitacoes_evasoes', @level2type=N'COLUMN',@level2name=N'crm_id'
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'origem do resgate (ted, prev, ota....)' , @level0type=N'SCHEMA',@level0name=N'silver', @level1type=N'TABLE',@level1name=N'fact_resgates', @level2type=N'COLUMN',@level2name=N'origem_resgate'
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'identificador da estrutura no sistema' , @level0type=N'SCHEMA',@level0name=N'silver', @level1type=N'TABLE',@level1name=N'fact_ativacoes_habilitacoes_evasoes', @level2type=N'COLUMN',@level2name=N'id_estrutura'
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'valor dos resgates brutos xp' , @level0type=N'SCHEMA',@level0name=N'silver', @level1type=N'TABLE',@level1name=N'fact_resgates', @level2type=N'COLUMN',@level2name=N'resgate_bruto_xp'
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'faixa de patrimônio líquido do cliente' , @level0type=N'SCHEMA',@level0name=N'silver', @level1type=N'TABLE',@level1name=N'fact_ativacoes_habilitacoes_evasoes', @level2type=N'COLUMN',@level2name=N'faixa_pl'
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'tipo de transferência de saida, nesse caso, todas sao saida' , @level0type=N'SCHEMA',@level0name=N'silver', @level1type=N'TABLE',@level1name=N'fact_resgates', @level2type=N'COLUMN',@level2name=N'tipo_transferencia'
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'tipo de movimentação do registro (ativação, habilitação ou evasão)' , @level0type=N'SCHEMA',@level0name=N'silver', @level1type=N'TABLE',@level1name=N'fact_ativacoes_habilitacoes_evasoes', @level2type=N'COLUMN',@level2name=N'tipo_movimentacao'
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'valor da transferência de saida. quanto de patrimonio o cliente tinha quando foi transferido para a M7' , @level0type=N'SCHEMA',@level0name=N'silver', @level1type=N'TABLE',@level1name=N'fact_resgates', @level2type=N'COLUMN',@level2name=N'resgate_bruto_transferencia'
-GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'resgate bruto total = valor dos resgates brutos xp + valor da transferência de saida' , @level0type=N'SCHEMA',@level0name=N'silver', @level1type=N'TABLE',@level1name=N'fact_resgates', @level2type=N'COLUMN',@level2name=N'resgate_bruto_total'
-GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'tabela silver que contém os dados de resgates e transferencias de saida. apenas os valores de saida são considerados nessa tabela' , @level0type=N'SCHEMA',@level0name=N'silver', @level1type=N'TABLE',@level1name=N'fact_resgates'
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'tabela fatos que contém os registros de ativação, habilitação e evasão de clientes' , @level0type=N'SCHEMA',@level0name=N'silver', @level1type=N'TABLE',@level1name=N'fact_ativacoes_habilitacoes_evasoes'
 GO
